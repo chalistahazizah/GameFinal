1ba6a4e50690119491aa025cabe9a43cf269b620
Lucyta Qutsyaning Rosydah <lucytaqr08@gmail.com>
2023-11-05 20:49:58 +0700

    add items & ui

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int apples = 0;
+
+    [SerializeField] private TMP_Text applesText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Apple"))
+        {
+            Destroy(collision.gameObject);
+            apples++;
+            applesText.text = "Apples: " + apples;
+        }
+    }
+
+}
